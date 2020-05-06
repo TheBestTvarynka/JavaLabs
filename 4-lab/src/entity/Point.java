3fa99fff6225f6efe672f81a5035ef173954ed99
pavlo myroniuk <pspos.developqkation@gmail.com>
2020-05-06 11:26:37 +0300

    added entury: Point

---
+++ 4-lab/src/entity/Point.java
@@ -0,0 +1,35 @@
+package entity;
+
+public class Point {
+    double x;
+    double y;
+
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        long longX = Double.doubleToLongBits(x);
+        long longY = Double.doubleToLongBits(y);
+        result = 31 * result + (int)(longX ^ (longX >>> 32));
+        result = 31 * result + (int)(longY ^ (longY >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return Double.compare(point.x, x) == 0 &&
+                Double.compare(point.y, y) == 0;
+    }
+}
