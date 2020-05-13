1b79f06c049aaed0f5dbee7dab97f50ab09a14cf
pavlo myroniuk <pspos.developqkation@gmail.com>
2020-05-13 12:02:40 +0300

    added logger for Validator

--- 5.1-lab/src/controller/Validator.java
+++ 5.1-lab/src/controller/Validator.java
@@ -3,8 +3,15 @@ package controller;
 import java.util.Calendar;
 import controller.exceptions.NotAmountException;
 import controller.exceptions.NotYearException;
+import model.FileIO;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
 public class Validator {
+    public static Logger logger = Logger.getLogger(FileIO.class);
+    static {
+        logger.setLevel(Level.DEBUG);
+    }
     public static void isNumber(String str) throws NumberFormatException {
         int asciiCode;
         for (int character = 0; character < str.length(); character++) {
@@ -13,6 +20,7 @@ public class Validator {
                 continue;
             }
             if (asciiCode < 48 || asciiCode > 57) {
+                logger.error("Entered value " + str + " is not number!");
                 throw new NumberFormatException("Entered value is not number!");
             }
         }
@@ -23,9 +31,11 @@ public class Validator {
             int year = Integer.parseInt(yearStr);
             final int currentYear = Calendar.getInstance().get(Calendar.YEAR);
             if (year < 0 || year > currentYear) {
+                logger.error("Entered year " + yearStr + " is not valid!");
                 throw new NotYearException("Entered year is not valid!");
             }
         } catch (NumberFormatException ex) {
+            logger.error("Entered year " + yearStr + " is not a number!");
             throw new NotYearException("Entered year is not a number!");
         }
         return null;
@@ -35,9 +45,11 @@ public class Validator {
             Validator.isNumber(amountStr);
             int amount = Integer.parseInt(amountStr);
             if (amount < 1) {
+                logger.error("Entered amount " + amountStr + " is not valid!");
                 throw new NotAmountException("Entered amount is not valid!");
             }
         } catch (NumberFormatException ex) {
+            logger.error("Entered year " + amountStr + " is not a number!");
             throw new NotAmountException("Entered amount is not a number!");
         }
         return null;
--- 5.1-lab/src/model/FileIO.java
+++ 5.1-lab/src/model/FileIO.java
@@ -2,6 +2,7 @@ package model;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 import java.io.FileNotFoundException;
@@ -12,40 +13,44 @@ import java.nio.file.Paths;
 
 public class FileIO {
     public static Logger logger = Logger.getLogger(FileIO.class);
+    static {
+        logger.setLevel(Level.DEBUG);
+    }
     public static Book[] readBooksFromFile (String filename) throws IOException {
-        logger.debug("start reading from the file: " + filename);
+        logger.info("start reading from the file: " + filename);
         Gson gson = new Gson();
         Book[] books = null;
         try {
             Path path = Paths.get(filename);
-            books = gson.fromJson(Files.readString(path), Book[].class);
+            String fileData = Files.readString(path);
+            books = gson.fromJson(fileData, Book[].class);
         } catch (FileNotFoundException e) {
-            logger.debug(e.getMessage());
+            logger.error("Error: file " + filename + " not found!");
             throw new IOException("Error: file not found!");
         } catch (IOException e) {
-            logger.debug(e.getMessage());
+            logger.error("Error: unable to read from file!");
             throw new IOException("Error: unable to read from file!");
         } catch (JsonSyntaxException e) {
-            logger.debug(e.getMessage());
+            logger.error("Error: input file has broken syntax!");
             throw new IOException("Error: input file has broken syntax!");
         }
-        logger.debug("successful read data from the file " + filename);
+        logger.info("successful read data from the file " + filename);
         return books;
     }
     public static void writeBooksToFile (Book[] books, String filename) throws IOException {
-        logger.debug("start writing to the file: " + filename);
+        logger.info("start writing to the file: " + filename);
         Gson gson = new Gson();
         try {
             Path path = Paths.get(filename);
             String booksStr = gson.toJson(books);
             Files.write(path, booksStr.getBytes());
         } catch (FileNotFoundException ex) {
-            logger.debug(ex.getMessage());
+            logger.error("Error: file " + filename + " not found!");
             throw new IOException("Error: file not found!");
         } catch (IOException e) {
-            logger.debug(e.getMessage());
+            logger.error("Error: unable to write in file!");
             throw new IOException("Error: unable to write in file!");
         }
-        logger.debug("successful write data to the file " + filename);
+        logger.info("successful write data to the file " + filename);
     }
 }
