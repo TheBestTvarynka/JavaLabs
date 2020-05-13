e6c85abf491b2726f00866985c6daa8802e3b623
pavlo myroniuk <pspos.developqkation@gmail.com>
2020-05-13 10:45:31 +0300

    added logger and base configuration for it

--- 5.1-lab/src/model/FileIO.java
+++ 5.1-lab/src/model/FileIO.java
@@ -13,30 +13,39 @@ import java.nio.file.Paths;
 public class FileIO {
     public static Logger logger = Logger.getLogger(FileIO.class);
     public static Book[] readBooksFromFile (String filename) throws IOException {
+        logger.debug("start reading from the file: " + filename);
         Gson gson = new Gson();
         Book[] books = null;
         try {
             Path path = Paths.get(filename);
             books = gson.fromJson(Files.readString(path), Book[].class);
         } catch (FileNotFoundException e) {
+            logger.debug(e.getMessage());
             throw new IOException("Error: file not found!");
         } catch (IOException e) {
+            logger.debug(e.getMessage());
             throw new IOException("Error: unable to read from file!");
         } catch (JsonSyntaxException e) {
+            logger.debug(e.getMessage());
             throw new IOException("Error: input file has broken syntax!");
         }
+        logger.debug("successful read data from the file " + filename);
         return books;
     }
     public static void writeBooksToFile (Book[] books, String filename) throws IOException {
+        logger.debug("start writing to the file: " + filename);
         Gson gson = new Gson();
         try {
             Path path = Paths.get(filename);
             String booksStr = gson.toJson(books);
             Files.write(path, booksStr.getBytes());
         } catch (FileNotFoundException ex) {
+            logger.debug(ex.getMessage());
             throw new IOException("Error: file not found!");
         } catch (IOException e) {
+            logger.debug(e.getMessage());
             throw new IOException("Error: unable to write in file!");
         }
+        logger.debug("successful write data to the file " + filename);
     }
 }
