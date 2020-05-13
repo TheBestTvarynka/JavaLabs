613f75e2f7fc6d2eb04ef858bb7fa3f8c90a92c8
pavlo myroniuk <pspos.developqkation@gmail.com>
2020-05-13 10:03:59 +0300

    initialize 5.1-lab

---
+++ 5.1-lab/src/controller/exceptions/NotAmountException.java
@@ -0,0 +1,10 @@
+package controller.exceptions;
+
+public class NotAmountException extends RuntimeException {
+    NotAmountException() {
+        super("Entered amount is not valid!");
+    }
+    public NotAmountException(String message) {
+        super(message);
+    }
+}
