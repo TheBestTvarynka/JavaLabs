5da47d488b050ea3f2d664b60824ee3a7b45708c
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2020-12-03 14:41:48 +0200

    feat(2.lab4): add entities

---
+++ 2-semester/TestWebJavaEE/src/com/kpi/lab4/entities/Request.java
@@ -0,0 +1,48 @@
+package kpi.java.entity;
+
+import kpi.java.enums.RoomType;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Request {
+    private UUID id;
+    private int seatNumber;
+    private RoomType type;
+    private Date dateFrom;
+    private Date dateTo;
+    private String phone;
+
+    public Request(UUID id, int seatNumber, RoomType type, Date dateFrom, Date dateTo, String phone) {
+        this.id = id;
+        this.seatNumber = seatNumber;
+        this.type = type;
+        this.dateFrom = dateFrom;
+        this.dateTo = dateTo;
+        this.phone = phone;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+
+    public RoomType getType() {
+        return type;
+    }
+
+    public Date getDateFrom() {
+        return dateFrom;
+    }
+
+    public Date getDateTo() {
+        return dateTo;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
